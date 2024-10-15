a669f93eb2665ef489b062f801ec7bbfa92e48ba
Alexcp13 <acp1320002@outlook.es>
2024-10-15 12:50:38 +0200

    s

--- app.js
+++ app.js
@@ -3,21 +3,22 @@ import dotenv from 'dotenv';
 import { corsMiddleware } from './middlewares/cors.js';
 import { PORT } from './config/port.config.js';
 import { connectDB } from './config/db.config.js';
-
 import { indexRoutes } from './routes/index.routes.js';
 import { errorHandler, errorRoute } from './err/index.js';
 
 
-const app = express();
-
-
 dotenv.config();
+
+const app = express();
 
 app.use(json());
 app.disable('x-powered-by')
 
 connectDB();
+
 app.use(corsMiddleware())
+
+
 
 app.use("/api", indexRoutes);
 
