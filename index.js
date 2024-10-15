6bca4cac284a9e9dc24bc4980c9f556879cce5a0
Alexcp13 <acp1320002@outlook.es>
2024-10-15 17:49:34 +0200

    S

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 import express, { json } from 'express';
 import dotenv from 'dotenv';
 import { corsMiddleware } from './middlewares/cors.js';
-import { PORT } from './config/port.config.js';
+
 import { connectDB } from './config/db.config.js';
 import { indexRoutes } from './routes/index.routes.js';
 import { errorHandler } from './err/err.js';
@@ -30,7 +30,7 @@ app.get("*", (req, res, next) => {
 app.use(errorHandler);
 
 
-
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server listening on port http://localhost:${PORT}`);
 })
