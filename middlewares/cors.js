2ab96fcc341225024fc80ac56ea1f9fd8687a0fa
Alexcp13 <acp1320002@outlook.es>
2024-10-17 13:35:34 +0200

    s

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -11,15 +11,13 @@ const ACCEPTED_ORIGINS = [
 
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
-
     origin: (origin, callback) => {
-        if (acceptedOrigins.includes(origin)) {
-            return callback(null, true)
+        if (acceptedOrigins.includes(origin) || !origin) {
+            return callback(null, true);
         }
-        if (!origin) {
-            return callback(null, true)
-
-        }
-        return callback(new Error('Origin not allowed by CORS'))
-    }
-})   
+        return callback(new Error('Origin not allowed by CORS'));
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204
+});
